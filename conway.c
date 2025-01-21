c747f94b6b65991c716b591e33828fe973467c97
trCVZ <tristan.cvz.pro@gmail.com>
2025-01-21 14:27:35 +0100

    Refactor Conway's Game of Life implementation to use a Cell struct for better state management and update logic

--- conway.c
+++ conway.c
@@ -1,43 +1,82 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define WIDTH 50
 #define HEIGHT 20
 
-typedef enum {
-    BACKGROUND = '-',
-    CELL = '$'
-} Grid;
+Cell grid[HEIGHT][WIDTH];
 
-void initGrid(char grid[HEIGHT][WIDTH]) {
+typedef enum {
+    DEAD,
+    ALIVE
+} CellState;
+
+typedef struct {
+    char symbol;
+    CellState state;
+} Cell;
+
+void initGrid(Cell grid[HEIGHT][WIDTH]) {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            grid[y][x] = '-';
+            grid[y][x].symbol = '-';
+            grid[y][x].state = DEAD;
         }
     }
 }
 
-void printGrid(char grid[HEIGHT][WIDTH]) {
+void printGrid(Cell grid[HEIGHT][WIDTH]) {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            printf("%c", grid[y][x]);
+            printf("%c", grid[y][x].symbol);
         }
         printf("\n");
     }
 }
 
+Cell updateCell(Cell cell, int y, int x) {
+    int ymax = y + 3;
+    int xmax = x + 3;
+    int countCellsAlive = 0;
+    for (y - 1; y < ymax; y++) {
+        for (x - 1; x < xmax; x++) {
+            if (grid[y][x].state == ALIVE) {
+                countCellsAlive++;
+            }
+        }
+    }
+    if (cell.state == ALIVE && countCellsAlive >= 2 && countCellsAlive > 3) {
+        cell.state = DEAD;
+        cell.symbol = '-';
+        return cell;
+    }
+    else if (cell.state == DEAD && countCellsAlive == 3) {
+        cell.state = ALIVE;
+        cell.symbol = '$';
+        return cell;
+    }
+    return cell;
+}
+
+
+void updateGrid(Cell grid[HEIGHT][WIDTH]) {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            grid[y][x] = updateCell(grid[y][x], x, y);
+        }
+    }
+}
+
+
 int main() {
 
-    char grid[HEIGHT][WIDTH];
     initGrid(grid);
 
+    Cell test;
+    test.state = DEAD;
 
-    grid[HEIGHT / 2][WIDTH / 2] = '$';
+    grid[HEIGHT / 2][WIDTH / 2].symbol = '$';
     printGrid(grid);
-
-
-
-
-
 
     return 0;
 }
